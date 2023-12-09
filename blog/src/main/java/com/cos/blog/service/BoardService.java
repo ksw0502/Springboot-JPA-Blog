07bc0238cbb58c55265e5ab193f89651e91e7d67
ksw0502 <asdfgyuio123@naver.com>
2023-12-10 00:19:27 +0900

    댓글 삭제완료

--- blog/src/main/java/com/cos/blog/service/BoardService.java
+++ blog/src/main/java/com/cos/blog/service/BoardService.java
@@ -7,14 +7,24 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.cos.blog.dto.ReplySaveRequestDto;
 import com.cos.blog.model.Board;
+import com.cos.blog.model.Reply;
 import com.cos.blog.model.User;
 import com.cos.blog.repository.BoardRepository;
+import com.cos.blog.repository.ReplyRepository;
+import com.cos.blog.repository.UserRepository;
 
 @Service
 public class BoardService {
 	@Autowired
 	private BoardRepository boardRepository;
+	
+	@Autowired
+	private ReplyRepository replyRepository;
+	
+	@Autowired
+	private UserRepository userRepository;
 	
 	@Transactional
 	public void 글쓰기(Board board,User user) {
@@ -47,5 +57,28 @@ public class BoardService {
 		});
 		board.setTitle(requestBoard.getTitle());
 		board.setContent(requestBoard.getContent());
+	}
+	
+	@Transactional
+	public void 댓글쓰기(ReplySaveRequestDto replySaveRequestDto) {
+		
+		User user = userRepository.findById(replySaveRequestDto.getUserId()).orElseThrow(()->{
+			return new IllegalArgumentException("댓글 쓰기 실패:게시글 아이디를 찾을 수 없습니다.");
+		});
+		
+		Board board = boardRepository.findById(replySaveRequestDto.getBoardId()).orElseThrow(()->{
+			return new IllegalArgumentException("댓글 쓰기 실패:게시글 아이디를 찾을 수 없습니다.");
+		});
+		
+		
+		Reply reply = new Reply();
+		reply.update(user, board, replySaveRequestDto.getContent());
+	
+		replyRepository.save(reply);
+	}
+	
+	@Transactional
+	public void 댓글삭제(int replyId) {
+		replyRepository.deleteById(replyId);
 	}
 }
--- blog/src/main/resources/static/js/user.js
+++ blog/src/main/resources/static/js/user.js
@@ -22,8 +22,12 @@ let index = {
 			contentType:"application/json; charset=utf-8",
 			dataType:"json" //요청을 서버로해서 응답이 왔을 때 기본적으로 모든 것이 문자열(생긴게 json이라면)=>javascript오브젝트로변경
 		}).done(function(resp){
+			if(resp.status===500){
+				alert("회원가입에 실패하였습니다.")
+			}else{
 			alert("회원가입이 완료되었습니다.")
 			location.href="/";
+			}
 		}).fail(function(error){
 			alert(JSON.stringify(error));
 		});
